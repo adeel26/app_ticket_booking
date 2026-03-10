381b09a62e46f093b4ceed205382a1b6a6d6be86
Adeel Iqbal <iadeel26@gmail.com>
2026-03-10 12:25:22 -0700

    Util: User Service Util

--- src/main/java/ticket/booking/services/UserBookingService.java
+++ src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,12 +1,36 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+import ticket.booking.util.UserServiceUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
 
     private User user;
 
-    public UserBookingService(User user) {
+    private List<User> userList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String USERS_PATH = "src/main/java/ticket/booking/localDb/users.json";
+
+
+    public UserBookingService(User user) throws IOException {
         this.user = user;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    }
+
+    public boolean loginUser() {
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPass
+        })
     }
 }

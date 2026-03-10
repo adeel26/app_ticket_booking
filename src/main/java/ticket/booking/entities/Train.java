0cfabb86d0229c08646aadae3ae1065cd14165b4
Adeel Iqbal <iadeel26@gmail.com>
2026-03-10 12:12:16 -0700

    Entities: Train

--- src/main/java/ticket/booking/entities/Train.java
+++ src/main/java/ticket/booking/entities/Train.java
@@ -16,4 +16,56 @@ public class Train {
 
      private List<String> stations;
 
+    public Train(String trainId, String trainNo, Map<String, Time> stationsTimes, List<String> stations, List<List<Integer>> seats) {
+        this.trainId = trainId;
+        this.trainNo = trainNo;
+        this.stationsTimes = stationsTimes;
+        this.stations = stations;
+        this.seats = seats;
+    }
+
+    public String getTrainId() {
+        return this.trainId;
+    }
+
+    public String getTrainNo() {
+        return this.trainNo;
+    }
+
+    public List<List<Integer>> getSeats() {
+        return this.seats;
+    }
+
+    public Map<String, Time> getStationsTimes() {
+        return this.stationsTimes;
+    }
+
+    public List<String> getStations() {
+        return this.stations;
+    }
+
+
+    public void setTrainId(String trainId) {
+        this.trainId = trainId;
+    }
+
+    public void setTrainNo(String trainNo) {
+        this.trainNo = trainNo;
+    }
+
+    public void setSeats(List<List<Integer>> seats) {
+        this.seats = seats;
+    }
+
+    public void setStationsTimes(Map<String, Time> stationsTimes) {
+        this.stationsTimes = stationsTimes;
+    }
+
+    public void setStations(List<String> stations) {
+        this.stations = stations;
+    }
+
+    public String getTrainInfo() {
+        return String.format("Train ID: %s Train No: %s", trainId, trainNo);
+    }
 }

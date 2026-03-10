97d5346fd6bb55c2ec56e534674cb278d1e0ded0
Adeel Iqbal <iadeel26@gmail.com>
2026-03-09 18:06:53 -0700

    Starting Ticket Booking App project

---
+++ src/main/java/ticket/booking/entities/User.java
@@ -0,0 +1,22 @@
+package ticket.booking.entities;
+
+import java.util.List;
+
+public class User {
+
+    private String name;
+
+    private String password;
+
+    private String hashPassword;
+
+    private List<Ticket> ticketsBooked;
+
+    private String userID;
+
+}
+
+/*
+git config user.name "Adeel Iqbal"
+git config user.email "iadeel26@gmail.com"
+ */

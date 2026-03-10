6646593da0f78eae7c1d6f06cadc66b300667df4
Adeel Iqbal <iadeel26@gmail.com>
2026-03-10 12:12:40 -0700

    Entities: User

--- src/main/java/ticket/booking/entities/User.java
+++ src/main/java/ticket/booking/entities/User.java
@@ -14,4 +14,61 @@ public class User {
 
     private String userID;
 
+    public User(String name, String password, String hashPassword, List<Ticket> ticketsBooked, String userID) {
+        this.name = name;
+        this.password = password;
+        this.hashPassword = hashPassword;
+        this.ticketsBooked = ticketsBooked;
+        this.userID = userID;
+    }
+
+    public User() {
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getHashPassword() {
+        return this.hashPassword;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return this.ticketsBooked;
+    }
+
+    public String getUserID() {
+        return this.userID;
+    }
+
+    public void printTickets() {
+        for (int i = 0; i < ticketsBooked.size(); i++) {
+            System.out.println(ticketsBooked.get(i).getTicketInfo());
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+
+    public void setTicketsBooked(List<Ticket> ticketsBooked) {
+        this.ticketsBooked = ticketsBooked;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
 }

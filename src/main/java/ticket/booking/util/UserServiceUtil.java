1fa6a73ee3e9446d4f8292d69c25d8d5bfefb937
Adeel Iqbal <iadeel26@gmail.com>
2026-03-12 02:24:51 -0700

    Hashed password and check password function added using lib Bcrypt

--- src/main/java/ticket/booking/util/UserServiceUtil.java
+++ src/main/java/ticket/booking/util/UserServiceUtil.java
@@ -1,5 +1,15 @@
 package ticket.booking.util;
 
+
+import org.mindrot.jbcrypt.BCrypt;
+
 public class UserServiceUtil {
 
+    public static String hashPassword(String planPassword) {
+        return BCrypt.hashpw(planPassword, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String plainPassword, String hashedPassword) {
+        return BCrypt.checkpw(plainPassword, hashedPassword);
+    }
 }

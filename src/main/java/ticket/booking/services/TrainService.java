933ee840426ca586ff7f5ce8dd08c18cc08fdbc5
Adeel Iqbal <iadeel26@gmail.com>
2026-03-15 12:02:42 -0700

    Features updated in Train Service

--- src/main/java/ticket/booking/services/TrainService.java
+++ src/main/java/ticket/booking/services/TrainService.java
@@ -1,4 +1,47 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.entities.Train;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class TrainService {
+
+    private List<Train> trainList;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final String TRAIN_DB_PATH = "src/main/java/ticket/booking/localDb/trains.json";
+
+    public TrainService() throws IOException {
+        File trains = new File(TRAIN_DB_PATH);
+        trainList = objectMapper.readValue(trains, new TypeReference<List<Train>>() {});
+    }
+
+    public List<Train> searchTrains(String source, String destination) {
+        return trainList.stream().filter(train -> validTrain(train, source, destination)).collect(Collectors.toList());
+    }
+
+    private boolean validTrain(Train train, String source, String destination) {
+        List<String> stationOrder = train.getStations();
+
+        int sourceIndex = stationOrder.indexOf(source.toLowerCase());
+        int destinationIndex = stationOrder.indexOf(destination.toLowerCase());
+
+        return sourceIndex != -1 && destinationIndex != -1 && sourceIndex < destinationIndex;
+    }
+
+    public void addTrain(Train newTrain) {
+        // Check if a train with the same trainID already exists
+        Optional<Train> existingTrain = trainList.stream()
+                .filter(train -> train.getTrainId().equalsIgnoreCase(newTrain.getTrainId())).findFirst();
+
+        if(existingTrain.isPresent()) {
+          // If train with the same trainId exists, update it instead of adding a new one
+          up
+        }
+    }
 }
